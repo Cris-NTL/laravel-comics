b2b83dac82ca499b0e5ad6fce0c7fe36917bb63d
Cris-NTL <natolicristian92@gmail.com>
2023-05-23 19:12:21 +0200

    Add: cards details

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="comics-container py-4">
+    <div class="comics-container">
         <div class="container">
-            <h2>Current Series</h2>
-            <div class="row row-cols-6 g-3">
+            <h3 class="px-4 py-1 m-0">CURRENT SERIES</h3>
+            <div class="row row-cols-6 g-3 pt-5">
                 @foreach ($books as $book)
                     <div class="col">
-                        <div class="card">
-                            <img src="{{ $book['thumb'] }}" alt="book">
-                            <div class="card-body">
+                        <div class="card border-0 mb-3">
+                            <div class="img">
+                                <img src="{{ $book['thumb'] }}" alt="book">
+                            </div>
+                            <div class="card-body p-0">
                                 <h5 class="card-title">{{ $book['title'] }}</h5>
+                                <p class="card-title">{{ $book['series'] }}</p>
+                                <p class="card-title">{{ $book['type'] }}</p>
+                                <p class="card-title">{{ $book['price'] }}</p>
+                                <p class="card-title">{{ $book['description'] }}</p>
                             </div>
                         </div>
                     </div>
